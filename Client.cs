0874b0be8f69045d564bea13bdb7607e49cf2f78
dobrybajer <kamil.zakk@gmail.com>
2016-05-20 13:17:25 +0200

    Added connection to local server

--- Client.cs
+++ Client.cs
@@ -16,6 +16,7 @@ namespace vindinium
         private static void Main(string[] args)
         {
             var serverUrl = args.Length == 4 ? args[3] : "http://vindinium.org";
+            //var serverUrl = args.Length == 4 ? args[3] : "http://192.168.0.18:9000";
 
             //create the server stuff, when not in training mode, it doesnt matter what you use as the number of turns
             var serverStuff = new ServerStuff(args[0], args[1] != "arena", uint.Parse(args[2]), serverUrl, "m2");
