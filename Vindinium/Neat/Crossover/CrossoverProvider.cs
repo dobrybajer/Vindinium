e9935b753a856cc632f53508ea492c5f7d1c02d7
Michał Mierzyński <michal@rateitapp.com>
2016-05-21 21:31:46 +0200

    Fixed crossover

--- Vindinium/Neat/Crossover/CrossoverProvider.cs
+++ Vindinium/Neat/Crossover/CrossoverProvider.cs
@@ -27,16 +27,17 @@ namespace vindinium.NEAT.Crossover
             correlationResults = correlationProvider.CorrelateConnections(genotype1.GenomeConnection, genotype2.GenomeConnection);
             CrossoverMasterType crossoverMasterType;
             var crossoverMaster = ChooseCrossoverMaster(genotype1, genotype2, out crossoverMasterType);
+            var crossoverSecond = ChooseCrossoverSecond(genotype1, genotype2, crossoverMasterType);
             
             var offspringGenome = new Genotype
             {
                 GenomeConnection = new List<ConnectionGenesModel>(),
-                NodeGens = new List<NodeGenesModel>(crossoverMaster.NodeGens),
+                NodeGens = GetNodesForOffspring(crossoverMaster, crossoverSecond),
                 Value = crossoverMaster.Value
             };
-            offspringGenome.GenomeConnection.AddRange(GetConnectionsForCorrelationType(crossoverMasterType, CorrelationItemType.Match));
-            offspringGenome.GenomeConnection.AddRange(GetConnectionsForCorrelationType(crossoverMasterType, CorrelationItemType.Disjoint));
-            offspringGenome.GenomeConnection.AddRange(GetConnectionsForCorrelationType(crossoverMasterType, CorrelationItemType.Excess));
+            offspringGenome.GenomeConnection.AddRange(GetConnectionsForMatch(crossoverMasterType));
+            offspringGenome.GenomeConnection.AddRange(GetConnectionsForDisjoint());
+            offspringGenome.GenomeConnection.AddRange(GetConnectionsForExcess());
             return offspringGenome;
         }
 
@@ -52,12 +53,20 @@ namespace vindinium.NEAT.Crossover
             return crossoverMasterType == CrossoverMasterType.GenotypeOne ? genotype1 : genotype2;
         }
 
-        private List<ConnectionGenesModel> GetConnectionsForCorrelationType(CrossoverMasterType crossoverMasterType, CorrelationItemType correlationItemType)
+        private Genotype ChooseCrossoverSecond(Genotype genotype1, Genotype genotype2,
+            CrossoverMasterType crossoverMasterType)
+        {
+            if (crossoverMasterType == CrossoverMasterType.GenotypeOne)
+                return genotype2;
+            return genotype1;
+        }
+
+        private List<ConnectionGenesModel> GetConnectionsForMatch(CrossoverMasterType crossoverMasterType)
         {
             var connectionWithGivenType = new List<ConnectionGenesModel>();
             foreach (var correlationItem in correlationResults.CorrelationItems)
             {
-                if (correlationItem.CorrelationItemType == correlationItemType)
+                if (correlationItem.CorrelationItemType == CorrelationItemType.Match)
                 {
                     connectionWithGivenType.Add(crossoverMasterType == CrossoverMasterType.GenotypeOne
                         ? correlationItem.ConnectionGene1
@@ -65,6 +74,63 @@ namespace vindinium.NEAT.Crossover
                 }
             }
             return connectionWithGivenType;
+        }
+
+        private List<ConnectionGenesModel> GetConnectionsForDisjoint()
+        {
+            var connectionWithGivenType = new List<ConnectionGenesModel>();
+            foreach (var correlationItem in correlationResults.CorrelationItems)
+            {
+                if (correlationItem.CorrelationItemType == CorrelationItemType.Disjoint)
+                {
+                    connectionWithGivenType.Add(correlationItem.ConnectionGene1 ?? correlationItem.ConnectionGene2);
+                }
+            }
+            return connectionWithGivenType;
+        }
+
+        private List<ConnectionGenesModel> GetConnectionsForExcess()
+        {
+            var connectionWithGivenType = new List<ConnectionGenesModel>();
+            foreach (var correlationItem in correlationResults.CorrelationItems)
+            {
+                if (correlationItem.CorrelationItemType == CorrelationItemType.Excess)
+                {
+                    connectionWithGivenType.Add(correlationItem.ConnectionGene1 ?? correlationItem.ConnectionGene2);
+                }
+            }
+            return connectionWithGivenType;
+        }
+
+        private List<NodeGenesModel> GetNodesForOffspring(Genotype master, Genotype second)
+        {
+            var nodesForCorrelationType = new List<NodeGenesModel>();
+            foreach (var correlationItem in correlationResults.CorrelationItems)
+            {
+                switch (correlationItem.CorrelationItemType)
+                {
+                    case CorrelationItemType.Match:
+                        nodesForCorrelationType.Add(master.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene1.InNode));
+                        nodesForCorrelationType.Add(master.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene1.OutNode));
+                        break;
+                    case CorrelationItemType.Disjoint:
+                    case CorrelationItemType.Excess:
+                        if (correlationItem.ConnectionGene1 != null)
+                        {
+                            nodesForCorrelationType.Add(master.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene1.InNode));
+                            nodesForCorrelationType.Add(master.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene1.OutNode));
+                        }
+                        else
+                        {
+                            nodesForCorrelationType.Add(second.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene2.InNode));
+                            nodesForCorrelationType.Add(second.NodeGens.Find(n => n.NodeNumber == correlationItem.ConnectionGene2.OutNode));
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return nodesForCorrelationType;
         }
     }
 }
