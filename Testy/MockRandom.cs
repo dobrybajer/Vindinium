c8f0ba73da877b0d5a3713ba3e25616ae17c9b4d
Michał Mierzyński <michal@rateitapp.com>
2016-05-24 21:44:33 +0200

    Added IRandomGenerator interface and mock it in tests

---
+++ Testy/MockRandom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vindinium.NEAT;
+
+namespace Testy
+{
+    public class MockRandom : IRandomGenerator
+    {
+        public int[] ValueToReturn { get; set; }
+        private int index;
+        public int Next(int minValue, int maxValue)
+        {
+            return ValueToReturn[index++ % ValueToReturn.Length];
+        }
+    }
+}
