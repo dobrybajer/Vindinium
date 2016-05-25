0fce304611184479b3da9015647edf2dea7cbca0
Michał Mierzyński <michal@rateitapp.com>
2016-05-25 10:40:07 +0200

    Fixed genetic algorithm method

--- Vindinium/Neat/NeatGeneticAlgorithm.cs
+++ Vindinium/Neat/NeatGeneticAlgorithm.cs
@@ -34,13 +34,13 @@ namespace vindinium.NEAT
             };
         }
 
-        public List<Genotype> CreateNewPopulationWithMutation(List<Genotype> genotypes,ref List<Innovations> innovationsList)
+        public List<Genotype> CreateNewPopulationWithMutation(List<Genotype> genotypes, ref List<Innovations> innovationsList)
         {
             var outputPopulation = new List<Genotype>();
             var random = new XorShiftRandom();
             var maxValue = genotypes.Max(g => g.Value);
             var probabilities = new List<double>(genotypes.Count);
-            probabilities.AddRange(genotypes.Select(genotype => genotype.Value/maxValue));
+            probabilities.AddRange(genotypes.Select(genotype => genotype.Value / maxValue));
 
             var roulette = new DiscreteDistribution(probabilities.ToArray());
 
@@ -49,10 +49,9 @@ namespace vindinium.NEAT
             for (int i = 0; i < attemptsCount; i++)
             {
                 var genomeId = DiscreteDistributionUtils.Sample(roulette, random);
-                roulette.RemoveOutcome(genomeId);
-                if(mutatedGenomesId.Contains(genomeId)) continue;;
+                roulette = roulette.RemoveOutcome(genomeId);
                 mutatedGenomesId.Add(genomeId);
-                outputPopulation.Add(mutationProvider.Mutate(genotypes[genomeId], NodeGeneParameters,ref innovationsList));
+                outputPopulation.Add(mutationProvider.Mutate(genotypes[genomeId], NodeGeneParameters, ref innovationsList));
             }
 
             for (int i = 0; i < genotypes.Count; i++)
@@ -80,15 +79,15 @@ namespace vindinium.NEAT
             for (int i = 0; i < attemptsCount; i++)
             {
                 var parentOneId = DiscreteDistributionUtils.Sample(roulette, random);
-                while (crossoveredGenomesId.Contains(parentOneId))
-                    parentOneId = DiscreteDistributionUtils.Sample(roulette, random);
-                roulette.RemoveOutcome(parentOneId);
+
+                roulette = roulette.RemoveOutcome(parentOneId);
                 var parentTwoId = DiscreteDistributionUtils.Sample(roulette, random);
-                while (crossoveredGenomesId.Contains(parentTwoId))
-                    parentTwoId = DiscreteDistributionUtils.Sample(roulette, random);
-                roulette.RemoveOutcome(parentTwoId);
-                crossoveredGenomesId.Add(parentOneId);
-                crossoveredGenomesId.Add(parentTwoId);
+                roulette = roulette.RemoveOutcome(parentTwoId);
+
+                if (genotypes[parentOneId].Value < genotypes[parentTwoId].Value)
+                    crossoveredGenomesId.Add(parentOneId);
+                else
+                    crossoveredGenomesId.Add(parentTwoId);
                 outputPopulation.Add(crossoverProvider.CrossoverGenotype(genotypes[parentOneId], genotypes[parentTwoId]));
             }
 
